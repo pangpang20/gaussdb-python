13e4f103eeb7e5886e5105c383538c441d5f14f5
5xuanwen <chenweikaiswy@outlook.com>
2025-04-27 11:41:00 +0800

    refactor: replace the skip condition with determining the sysadmin instead of superuser.

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -405,8 +405,9 @@ class TestLiteral:
     @pytest.mark.crdb_skip("composite")  # create type, actually
     @pytest.mark.parametrize("name", ["a-b", f"{eur}", "order", "foo bar"])
     def test_invalid_name(self, conn, name):
-        if conn.info.parameter_status("is_superuser") != "on":
-            pytest.skip("not a superuser")
+        is_sysadmin = conn.execute("select 1 from pg_roles where rolname = current_user and rolsystemadmin = 't'")
+        if not is_sysadmin.fetchone():
+            pytest.skip("not a sysadmin")
         conn.execute(
             f"""
             set client_encoding to utf8;
