2f41c1de310a8e7fe22fa801402c3c20fd510d4f
pangpang20 <676814828@qq.com>
2025-05-18 16:42:26 +0800

    Add gaussdb_skip for test_max_lifetime due to backend pid unsupported

--- tests/pool/test_pool_async.py
+++ tests/pool/test_pool_async.py
@@ -693,6 +693,7 @@ async def test_bad_resize(dsn, min_size, max_size):
 
 @pytest.mark.slow
 @pytest.mark.timing
+@pytest.mark.gaussdb_skip("backend pid")
 @pytest.mark.crdb_skip("backend pid")
 async def test_max_lifetime(dsn):
     async with pool.AsyncConnectionPool(dsn, min_size=1, max_lifetime=0.2) as p:
