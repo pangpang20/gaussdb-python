0e55196289166008b4a49406deec0f685c5be2b1
pangpang20 <676814828@qq.com>
2025-05-18 11:30:22 +0800

    Add gaussdb_skip for test_getconn_check due to pg_terminate_backend unsupported

--- tests/pool/test_pool.py
+++ tests/pool/test_pool.py
@@ -784,6 +784,7 @@ def test_connect_check(dsn, caplog, autocommit):
 
 @pytest.mark.parametrize("autocommit", [True, False])
 @pytest.mark.crdb_skip("pg_terminate_backend")
+@pytest.mark.gaussdb_skip("pg_terminate_backend")
 def test_getconn_check(dsn, caplog, autocommit):
     caplog.set_level(logging.WARNING, logger="psycopg.pool")
 
